e9d90a40d332e45a6d2bd6f9ee58f1693f294282
JimeTg98 <jimenatg98@gmail.com>
2025-03-29 17:58:35 -0600

    menu_pausa

---
+++ Assets/Scripts/Botones.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class Botones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Jugar();
+        Salir();
+        Menu();
+        Créditos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Jugar()
+    {
+
+        SceneManager.LoadScene("Level1");
+        Time.timeScale = 1f;
+
+    }
+
+
+
+   
+    public void Menu()
+    {
+
+        SceneManager.LoadScene("Menu_inicio");
+
+    }
+
+
+
+
+    public void Créditos()
+    {
+
+        SceneManager.LoadScene("Creditos");
+
+    }
+
+
+    public void Salir()
+    {
+
+        Application.Quit();
+        Debug.Log("salio del juego");
+    }
+
+
+}

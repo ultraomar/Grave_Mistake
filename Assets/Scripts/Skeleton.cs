22a83b40ccb5587cd65ab418d4ccd01a460e8d5c
ultraomar <ultraomar@users.noreply.github.com>
2025-04-01 23:10:02 -0600

    Added Skeletons as Scriptable Objects
    Added Skeletons as Scriptable Objects. A new scene was created for testing the scriptable objects.

--- Assets/Scripts/Skeleton.cs
+++ Assets/Scripts/Skeleton.cs
@@ -1,7 +1,28 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Skeleton", menuName = "Scriptable Objects/Skeleton")]
+[CreateAssetMenu(fileName = "New Skeleton", menuName = "Skeleton")]
 public class Skeleton : ScriptableObject
 {
-    
+    public int attack;
+    public int hp;
+    public Sprite handL;
+    public Sprite armLup;
+    public Sprite armLdwn;
+    public Sprite eyes;
+    public Sprite skull;
+    public Sprite ribbs;
+    public Sprite legLup;
+    public Sprite legLdwn;
+    public Sprite footL;
+    public Sprite pelv;
+    public Sprite spine;
+    public Sprite legRup;
+    public Sprite legRdwn;
+    public Sprite handR;
+    public Sprite armRup;
+    public Sprite armRdwn;
+    public Sprite footR;
+    public Sprite skull_back;
+    public Sprite weapon;
+    public Color skColor;
 }
